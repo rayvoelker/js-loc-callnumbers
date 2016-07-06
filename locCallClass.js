71a294f13981917ab4554ce65f044546befa0734
Ray Voelker <ray.voelker@gmail.com>
2016-07-06 13:49:23 -0400

    made major changes to padZed and returnNormLcCall
    The changes should now properly sort a wider range of library of congress call numbers correctly.
    
    Numbers before the first cutter are now broken up into two components -- whole number and decimal and padded differently:
    for example, the number: 
    761.9 
    is padded out to this:
    000000761.900000000
    when it comes before the first cutter
    
    Any number or letters after the first cutter are padded on the right as in the following example:
    .W93
    W00000000.930000000

--- locCallClass.js
+++ locCallClass.js
@@ -18,55 +18,73 @@ function locCallClass () {
 locCallClass.prototype.returnNormLcCall = function (call_number) {
 	var result = this.lc.exec(call_number);
 	if (!result) {
+		
 		return 0;
+		
 	}
 	
+	//track the position of what we're looking at in the callnumber 
+	var before_first_cutter = true;
 	var return_string = "";
 	
 	//work through the results starting at 1 (the 0 value in the array is the original input)
 	for(var i=1; i<=(result.length-1); i++) {
 		
 		if (i>1) {
+			
 			return_string = return_string + "."
+			
 		}
 		
-		//right pad all letters in the call number ... left pad all numbers
-		if( isNaN(result[i]) ) {
-			return_string = return_string + this.padZed(result[i], 'right');
+		if (i>2) {
+			
+			before_first_cutter = false;
+			
 		}
-		else {
-			return_string = return_string + this.padZed(result[i], 'left');
-		}
+		
+		return_string = return_string + this.padZed(result[i], before_first_cutter);
 		
 	}		
+	
 	return return_string;
+	
 }
 
 locCallClass.prototype.numStacks = function () {
+	
 	return this.stacks_data.length;
+	
 }
 
 //class method
 //Finds if a value "c" is between values "a" and "b"
 locCallClass.prototype.isBetween = function (a, b, c) {
+	
 	a_norm = this.returnNormLcCall(a);
 	b_norm = this.returnNormLcCall(b);
 	c_norm = this.returnNormLcCall(c);
 	
 	//first make sure that a comes before b ...
 	if (a_norm > b_norm) {
+		
 		console.log ("a bigger than b");
 		return false;
+		
 	}
+	
 	if ( (a_norm < c_norm) && (c_norm < b_norm) ) {
+		
 		return true;
+		
 	}
 	return false;
+	
 }
 
 //class method
 //takes a variable list of call numbers, and returns a sorted array of call numbers
 locCallClass.prototype.sortCallNumbers = function () {
+	
 	var values = [];
 	//console.log("number of arguments: " + arg)
 	
@@ -78,13 +96,20 @@ locCallClass.prototype.sortCallNumbers = function () {
 	
 	//sort the 2d array on the [0][] set of values
 	values.sort( function (a,b) {
+		
 		if (a[0] > b[0]) {
+			
 			return 1;
+			
 		}
 		if (a[0] < b[0]) {
+			
 			return -1;
+			
 		}
+		
 		return 0;
+		
 	});
 	
 	var return_array = [];
@@ -93,6 +118,7 @@ locCallClass.prototype.sortCallNumbers = function () {
 	}
 	
 	return return_array;
+	
 }
 
 // class method
@@ -104,20 +130,56 @@ locCallClass.prototype.sortCallNumbers = function () {
 // Returns :
 //	value -- padded part of call number 
 // pads a string value with 0s if the string value has a length less than zero
-locCallClass.prototype.padZed = function (value, direction) {
+locCallClass.prototype.padZed = function (value, before_first_cutter) {
 	//pad value with zeros - return value will have a length of 9 
-	var pad_zeros = "";
-	if (value) {
-		for (var i=0; i<(9 - value.length); i++) {
-			pad_zeros = pad_zeros + "0";
-		}
-		if (direction == 'right') {
-			return value + pad_zeros;
-		}
+	// The exceptions here are going to be if the number is before the 
+	// cutter, then we should treat it as two different parts: whole number,
+	// and decimal portion.
+
+	if(value) {
+		
+		if(before_first_cutter && !isNaN(value) ) {
+			
+			//this is a number before the first cutter, split it, and then
+			// pad each of the parts 
+			var int_portion = Math.floor(value).toString();
+			var dec_portion = (value % 1).toFixed(3).toString().substr(2,3);
+			
+			var pad_zeros = "";
+			for (var i=0; i<(9 - int_portion.length); i++) {
+				pad_zeros = pad_zeros + "0";
+			}
+			
+			return_value = pad_zeros + int_portion;
+			
+			var pad_zeros = "";
+			for (var i=0; i<(9 - dec_portion.length); i++) {
+				pad_zeros = pad_zeros + "0";
+			}
+			
+			return_value += "." + dec_portion + pad_zeros;
+			return return_value;
+			
+		} // end if
+	
 		else {
-			return pad_zeros + value;
-		}
-	}
-	else
+
+			//just pad the value to the right
+			var pad_zeros = "";
+			for (var i=0; i<(9 - value.length); i++) {
+				pad_zeros = pad_zeros + "0";
+			}
+
+			return value + pad_zeros;
+			
+		} //end else
+		
+	} //end if(value)
+	
+	else {
+		
 		return "000000000";
+		
+	}
+		
 }
