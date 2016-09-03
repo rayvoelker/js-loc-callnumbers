a138dac2e61b235defea7e1e6d44ba5d66c731c2
Ray Voelker <ray.voelker@gmail.com>
2016-09-03 13:46:42 -0400

    clean up and simplification
    Simplified / refactored some of the code changed some documentation, and removed unused method

--- locCallClass.js
+++ locCallClass.js
@@ -1,108 +1,66 @@
 // js-loc-callnumbers
-// 
-// project located at:
-// https://github.com/rayvoelker/js-loc-callnumbers
-//
-// A javascript class to normalize and perform various sorting and
-// comparison operations on Library of Congress Call Numbers within a 
-// library institution.
+// A javascript class to normalize and perform various sorting options on
+// Library of Congress Call Numbers within a library institution.
 
-// constructor function
-// Defines :
-//	this.stacks_data -- json object of the library stacks
-//	this.lc	-- regular expression that defines a Library of Congress Call Number
-function locCallClass () {
-	//the regular expression that defines the Library of Congress Call number
+function locCallClass() {
+	// the regular expression that defines the Library of 
+	// Congress call number. Thanks to Bill Dueber's post on the subject for the regex 
+	// http://robotlibrarian.billdueber.com/2008/11/normalizing-loc-call-numbers-for-sorting/
 	this.lc = /^\s*([A-Z]{1,3})\s*(\d+(?:\s*\.\s*\d+)?)?\s*(?:\.?\s*([A-Z]+)\s*(\d+)?)?(?:\.?\s*([A-Z]+)\s*(\d+)?)?\s*(.*?)\s*$/;
 }
 
-// class method
-// Takes :
-//	call_number -- Library of Congress Call Number
-// Returns :
-//	padded string value of the call number
-locCallClass.prototype.returnNormLcCall = function (call_number) {
+// locCallClass.returnNormLcCall(call_number)
+// returns a "normalized" call number
+locCallClass.prototype.returnNormLcCall = function(call_number) {
 	var result = this.lc.exec(call_number);
 	if (!result) {
-		
-		return 0;
-		
+		throw new Error('Not a Call Number');		
 	}
 	
-	//track the position of what we're looking at in the callnumber 
+	// track the position of what we're looking at in the callnumber 
 	var before_first_cutter = true;
 	var return_string = "";
 	
-	//work through the results starting at 1 (the 0 value in the array is the original input)
+	// work through the results starting at 1 (the 0 value in the array is the original input)
 	for(var i=1; i<=(result.length-1); i++) {
-		
 		if (i>1) {
-			
 			return_string = return_string + "."
-			
 		}
 		
 		if (i>2) {
-			
 			before_first_cutter = false;
-			
 		}
 		
 		return_string = return_string + this.padZed(result[i], before_first_cutter);
-		
 	}		
 	
+	// TODO: consider adding further checks to see if the return string 
+	// consists of 8 segments 9 characters and throw an error if not
 	return return_string;
-	
 }
 
-locCallClass.prototype.numStacks = function () {
-	
-	return this.stacks_data.length;
-	
-}
-
-//class method
-//Finds if a value "c" is between values "a" and "b"
-// also returns true if the "c" is equal to either "a" or "b"
+// locCallClass.isBetween(a, b, c)
+// returns true if a <= c <= b
 locCallClass.prototype.isBetween = function (a, b, c) {
+	var a_norm = this.returnNormLcCall(a),
+		b_norm = this.returnNormLcCall(b),
+		c_norm = this.returnNormLcCall(c);
 	
-	a_norm = this.returnNormLcCall(a);
-	b_norm = this.returnNormLcCall(b);
-	c_norm = this.returnNormLcCall(c);
-	
-	if (a_norm == c_norm) {
+	if ( (a_norm <= c_norm) && (c_norm <= b_norm) ) {
 		return true;
 	}
 	
-	if (b_norm == c_norm) {
-		return true;
-	}
-	
-	//first make sure that a comes before b ...
-	if (a_norm > b_norm) {
-		
-		//console.log ("a bigger than b");
+	else {
 		return false;
-		
 	}
-	
-	if ( (a_norm < c_norm) && (c_norm < b_norm) ) {
-		
-		return true;
-		
-	}
-	return false;
-	
 }
 
-//class method
-//takes a variable list of call numbers, and returns a sorted array of call numbers
+// locCallClass.sortCallNumbers()
+// takes a variable list of call numbers as arguments, and returns 
+// a sorted array of call numbers in their original format
 locCallClass.prototype.sortCallNumbers = function () {
-	
 	var values = [];
-	//console.log("number of arguments: " + arg)
-	
+		
 	for (var i=0; i<arguments.length; i++) {
 		values[i]=[];
 		values[i][0] = this.returnNormLcCall(arguments[i]);
@@ -111,20 +69,15 @@ locCallClass.prototype.sortCallNumbers = function () {
 	
 	//sort the 2d array on the [0][] set of values
 	values.sort( function (a,b) {
-		
 		if (a[0] > b[0]) {
-			
 			return 1;
-			
 		}
+		
 		if (a[0] < b[0]) {
-			
 			return -1;
-			
 		}
 		
 		return 0;
-		
 	});
 	
 	var return_array = [];
@@ -133,34 +86,24 @@ locCallClass.prototype.sortCallNumbers = function () {
 	}
 	
 	return return_array;
-	
 }
 
-// class method
-// Takes :
-//	value 		-- part of a call number to be padded with zeros
-//	direction	-- The direction of the padding ... 
-//					ie "right" "A" pads as "A00000000"
-//					"left" "35" pads as "000000035"
-// Returns :
-//	value -- padded part of call number 
-// pads a string value with 0s if the string value has a length less than zero
+// locCallClass.padZed()
+// returns portion of the call number padded out to enable sorting.
 locCallClass.prototype.padZed = function (value, before_first_cutter) {
 	//pad value with zeros - return value will have a length of 9 
 	// The exceptions here are going to be if the number is before the 
-	// cutter, then we should treat it as two different parts: whole number,
-	// and decimal portion.
+	// cutter, then we should treat it as two different parts: whole 
+	// number, and decimal portion.
 
 	if(value) {
-		
 		if(before_first_cutter && !isNaN(value) ) {
-			
 			//this is a number before the first cutter, split it, and then
 			// pad each of the parts 
 			var int_portion = Math.floor(value).toString();
 			var dec_portion = (value % 1).toFixed(3).toString().substr(2,3);
-			
 			var pad_zeros = "";
+			
 			for (var i=0; i<(9 - int_portion.length); i++) {
 				pad_zeros = pad_zeros + "0";
 			}
@@ -174,27 +117,20 @@ locCallClass.prototype.padZed = function (value, before_first_cutter) {
 			
 			return_value += "." + dec_portion + pad_zeros;
 			return return_value;
-			
 		} // end if
 	
 		else {
-
-			//just pad the value to the right
+			//pad the value to the right
 			var pad_zeros = "";
 			for (var i=0; i<(9 - value.length); i++) {
 				pad_zeros = pad_zeros + "0";
 			}
 
 			return value + pad_zeros;
-			
-		} //end else
-		
-	} //end if(value)
+		}
+	} 
 	
 	else {
-		
 		return "000000000";
-		
 	}
-		
 }
